0458796b2c338dc3a7a982e3d8d5a21655708858
null <null@localhost.localdomain>
2016-11-18 23:19:06 +0000

    created files for views

--- server/config/routes.rb
+++ server/config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'overviews#home'
+
+  #incidence
+  get    'incidences' => 'incidences#index'
+  get    'setting' => 'incidences#setting'
+  get    'today' => 'incidences#today'
+  get    'newest' => 'incidences#newest'
+  get    'all' => 'incidences#all'
+  get    'calendar' => 'incidences#calendar'
+  get    'search' => 'incidences#search'
 end
