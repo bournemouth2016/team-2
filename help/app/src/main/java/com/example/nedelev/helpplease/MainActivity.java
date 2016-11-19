92f72cbe184539c925456ad6395c1e9fe96d84fc
Peshowe <nedelevbg@gmail.com>
2016-11-19 04:05:25 +0000

    fw

--- help/app/src/main/java/com/example/nedelev/helpplease/MainActivity.java
+++ help/app/src/main/java/com/example/nedelev/helpplease/MainActivity.java
@@ -4,10 +4,12 @@ import java.util.ArrayList;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ToggleButton;
 
 
 public class MainActivity extends AppCompatActivity {
-
     private ArrayList<Integer> typeOfEmergency = new ArrayList<Integer>();
 
     private OnCheckedChangeListener sinking = new OnCheckedChangeListener() {
