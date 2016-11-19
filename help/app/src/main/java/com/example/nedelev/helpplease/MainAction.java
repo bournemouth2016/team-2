ca236b2f363fad4850696a7b29773f3ebc14ef59
Peshowe <nedelevbg@gmail.com>
2016-11-19 04:49:04 +0000

    fw

--- help/app/src/main/java/com/example/nedelev/helpplease/MainAction.java
+++ help/app/src/main/java/com/example/nedelev/helpplease/MainAction.java
@@ -4,16 +4,20 @@ import java.util.ArrayList;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.parse.Parse;
 
 
 public class MainAction extends AppCompatActivity {
-    private OnClickListener help = new OnClickListener() {
+    private View.OnClickListener help = new View.OnClickListener() {
         public void onClick(View v) {
             Intent intent = new Intent(this, Help.class);
             startActivity(intent);
         }
     };
-    private OnClickListener reg = new OnClickListener() {
+    private View.OnClickListener reg = new View.OnClickListener() {
         public void onClick(View v) {
             Intent intent = new Intent(this, Register.class);
             startActivity(intent);
@@ -24,7 +28,10 @@ public class MainAction extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-
+        Parse.initialize(new Parse.Configuration.Builder(this)
+                        .applicationId("MyAppId")
+                        .server("http://178.62.115.151:1337/parse")
+                        .build());
         Button helpMe = (Button)findViewById(R.id.helpButton);
         // Register the onClick listener with the implementation above
         helpme.setOnClickListener(this.help);
--- help/app/src/main/java/com/example/nedelev/helpplease/MainActivity.java
+++ help/app/src/main/java/com/example/nedelev/helpplease/MainActivity.java
@@ -71,6 +71,7 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.help);
+
         ToggleButton siking = (ToggleButton)findViewById(R.id.toggleButton);
         // Register the onClick listener with the implementation above
         sinking.setOnCheckedChangeListener(this.sinking);
