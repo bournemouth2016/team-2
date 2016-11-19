3492a338a4fa93a92834cbd0b5e3cece6530fa98
null <null@localhost.localdomain>
2016-11-19 01:13:29 +0000

    pushing to heroku

--- server/app/controllers/incidences_controller.rb
+++ server/app/controllers/incidences_controller.rb
@@ -1,2 +1,25 @@
 class IncidencesController < ApplicationController
+
+  def new
+    @incidence = Incidence.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def show
+    # @zone_items = current_user.reservation_of_zone(@incidence)
+  end
+
+  def index
+  end
+
+
+
 end
